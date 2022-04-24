e904c0abd708b258b129ca55e816bb8cac71fb80
trebordng <96870545+trebordng@users.noreply.github.com>
2022-04-25 04:41:47 +1000

    change image/next

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Layout } from "./Layout";
 export { default as Fancybox } from "./Fancybox";
 export { default as DisplayImage } from "./DisplayImage";
+export { default as AlbumSubpage } from "./AlbumSubpage";
