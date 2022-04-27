286c66b2d121d30ec0de3b9940b96f81de6d3989
trebordng <96870545+trebordng@users.noreply.github.com>
2022-04-27 21:43:49 +1000

    disable ssr index page

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.scss";
 import { Layout } from "../components";
+import NoSsr from "../components/NoSsr";
 import "../styles/landing-page.scss";
 import "../styles/header.scss";
 import "../styles/about.scss";
@@ -12,11 +13,11 @@ function MyApp({ Component, pageProps }) {
     import("bootstrap/dist/js/bootstrap");
   }, []);
   return (
-   
+    <NoSsr>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-   
+    </NoSsr>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import en from "../locales/en";
 import vie from "../locales/vie";
 import Fade from "react-reveal/Fade";
 import Image from "next/image";
-import NoSsr from "../components/NoSsr";
 
 const Index = () => {
   const router = useRouter();
@@ -14,32 +13,30 @@ const Index = () => {
   const t = locale === "en" ? en : vie;
 
   return (
-    <NoSsr>
-      <section className="landing-page">
-        <Fade duration={1500} delay={500}>
-          <header className="title">{t.pageTitle}</header>
-          <div className="subtitle">
-            <em>{t.pageAuthor}</em>
-          </div>
-          <article className="pretty-lady">
-            <Image
-              alt="pretty-lady"
-              src={portraitImage.src}
-              layout="fill"
-              objectFit="cover"
-              className="portrait-border"
-              quality={80}
-            />
-          </article>
+    <section className="landing-page">
+      <Fade duration={1500} delay={500}>
+        <header className="title">{t.pageTitle}</header>
+        <div className="subtitle">
+          <em>{t.pageAuthor}</em>
+        </div>
+        <article className="pretty-lady">
+          <Image
+            alt="pretty-lady"
+            src={portraitImage.src}
+            layout="fill"
+            objectFit="cover"
+            className="portrait-border"
+            quality={80}
+          />
+        </article>
 
-          <Link href="about">
-            <a>
-              <button>{t.open}</button>
-            </a>
-          </Link>
-        </Fade>
-      </section>
-    </NoSsr>
+        <Link href="about">
+          <a>
+            <button>{t.open}</button>
+          </a>
+        </Link>
+      </Fade>
+    </section>
   );
 };
 
